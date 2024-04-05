d9b0f6f0f4d51fb6057aa79c5d7ccdf532df659a
vnyambuti <vnyambuti@gmail.com>
2024-04-05 08:12:21 +0300

    creating shop and cart pages

--- lib/components/bottom_nav.dart
+++ lib/components/bottom_nav.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNav extends StatelessWidget {
-  const BottomNav({super.key});
+  void Function(int)?onTabChange;
+   BottomNav({super.key,required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return  Container(
       margin: const EdgeInsets.all(25),
       child:  GNav(
+        onTabChange: (value)=>onTabChange!(value),
         color: Colors.grey[400],
         mainAxisAlignment: MainAxisAlignment.center,
         activeColor: Colors.grey[700],
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,45 @@
 import 'package:coffee/components/bottom_nav.dart';
 import 'package:coffee/const.dart';
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'cart-page.dart';
+
+import 'shop-page.dart';
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+  
+}
+
+
+class _HomePageState extends State<HomePage> {
+  int _selectedindex=0;
+
+
+
+final List<Widget> _pages=[
+  ShopPage(),
+
+  CartPage()
+];
+  void navigate(int index){
+setState((){
+  _selectedindex=index;
+});
+}
+  @override
+
   Widget build(BuildContext context) {
-    return Scaffold(
+     return Scaffold(
         backgroundColor: bg,
-        bottomNavigationBar: BottomNav(),
+        bottomNavigationBar: BottomNav(
+          onTabChange: (index) => navigate(index),
+        ),
+        body: _pages[_selectedindex],
     );
   }
-}
+  
+  }
